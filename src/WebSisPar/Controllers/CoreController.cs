b6551b6ac2aebffdf46506f5c43e511b9907b7b6
muratgoktas <mcs.goktas@gmail.com>
2024-06-08 20:36:40 +0300

    07- Tamplate Web Page created. Core web page created.

---
+++ src/WebSisPar/Controllers/CoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSisPar.Controllers
+{
+    public class CoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
